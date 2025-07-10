fe28d20386d04b8d0f9b97ca8c894f7d3dfcd18a
John Ngo <jngo@users.noreply.github.com>
2025-07-10 09:47:24 +0200

    Revised job experience section and added random projects section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,76 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Mail, Linkedin, Github, Twitter } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="container mx-auto px-4 py-16 max-w-4xl">
+      {/* Hero Section */}
+      <header className="mb-16">
+        <h1 className="text-4xl font-bold mb-6">
+          Hi, I’m John.
+        </h1>
+        <p className="text-xl mb-8 leading-relaxed">
+          Designer by practice, engineer by training, researcher at heart. I tackle complex product challenges by combining technical depth with user-centered design, turning ambiguity into actionable insights and shipped solutions.
+        </p>
+      </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <section className="mb-16">
+        <h2 className="font-bold mb-2">Experience</h2>
+        <ul>
+          <li className="flex items-center gap-4 border-t py-1">
+            <span><a href="https://www.mckinsey.com/" className="text-muted-foreground font-bold hover:bg-muted">McKinsey & Company</a></span>
+            <span className="grow">Leading design across digital transformation initiatives.</span>
+            <span className="w-1/8 text-right text-muted-foreground">2021–Present</span>
+          </li>
+          <li className="flex items-center gap-4 border-t py-1">
+            <span><a href="https://up42.com/" className="text-muted-foreground font-bold hover:bg-muted">UP42</a></span>
+            <span className="grow">Established two-person design practice and launched several keystone projects.</span>
+            <span className="w-1/8 text-right text-muted-foreground">2019–2021</span>
+          </li>
+          <li className="flex items-center gap-4 border-t py-1">
+            <span><a href="https://www.candis.io/" className="text-muted-foreground font-bold hover:bg-muted">Candis</a></span>
+            <span className="grow">Design team of one, hands-on across end-to-end from research to React.js.</span>
+            <span className="w-1/8 text-right text-muted-foreground">2017–2019</span>
+          </li>
+          <li className="flex items-center gap-4 border-t py-1">
+            <span><a href="https://urbansportsclub.com/" className="text-muted-foreground font-bold hover:bg-muted">Urban Sports Club</a></span>
+            <span className="grow">Laid the technical and product foundations for European expansion.</span>
+            <span className="w-1/8 text-right text-muted-foreground">2015</span>
+          </li>
+        </ul>
+      </section>
+
+      <section className="mb-16">
+        <h2 className="font-bold mb-2">Random</h2>
+        <ul>
+          <li className="flex items-center gap-4 border-t py-1">
+            <span><a href="https://synthesiser.smallepic.com" className="text-muted-foreground font-bold hover:bg-muted">Synthesiser</a></span>
+            <span className="grow">Generate a Minto Pyramid synthesis of any content.</span>
+          </li>
+          <li className="flex items-center gap-4 border-t py-1">
+            <span><a href="https://podscriber.smallepic.com/" className="text-muted-foreground font-bold hover:bg-muted">Podscriber</a></span>
+            <span className="grow">Instant transcribe podcast episodes and send them to your read-it-later queue.</span>
+          </li>
+          <li className="flex items-center gap-4 border-t py-1">
+            <span><a href="https://mermaid.smallepic.com/" className="text-muted-foreground font-bold hover:bg-muted">Mermaid Viewer</a></span>
+            <span className="grow">A lightweight, mobile-friendly Mermaid diagram viewer.</span>
+          </li>
+          <li className="flex items-center gap-4 border-t py-1">
+            <span><a href="http://filmsandconversations.com/" className="text-muted-foreground font-bold hover:bg-muted">Films & Conversations</a></span>
+            <span className="grow">A monthly film club bringing together people, documentaries, and discussions.</span>
+          </li>
+          <li className="flex items-center gap-4 border-t py-1">
+            <span><a href="http://booksandconversations.com/" className="text-muted-foreground font-bold hover:bg-muted">Books & Conversations</a></span>
+            <span className="grow">Roundtable discussions with good friends and great books.</span>
+          </li>
+        </ul>
+      </section>
+
+      <footer className="mt-16">
+        <h2 className="font-bold mb-2">Reach Out</h2>
+        <p>Send me an <a href="mailto:john@smallepic.com" className="text-muted-foreground font-bold hover:bg-muted">email</a>, or find me on <a href="https://www.linkedin.com/in/jngo/" className="text-muted-foreground font-bold hover:bg-muted">LinkedIn</a>, <a href="https://github.com/jngo" className="text-muted-foreground font-bold hover:bg-muted">GitHub</a>, or <a href="https://twitter.com/jngo" className="text-muted-foreground font-bold hover:bg-muted  ">Twitter</a>.</p>
       </footer>
     </div>
   );
